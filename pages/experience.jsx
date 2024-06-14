5c95059062616a7a20f5dac8d36f292b1f210e41
jeremykenneth7 <jeremykenneth7@email.com>
2024-06-14 17:11:18 +0700

    add experiences

--- pages/experience.jsx
+++ pages/experience.jsx
@@ -42,63 +42,92 @@ export default function Experience() {
                     </div>
                 ) : (
                     <>
-                <div className="p-10 py-10 flex flex-col md:flex-row items-start md:items-center">
-                    <div>
-                        <a
-                            href="https://grow.google/intl/id_id/bangkit/?tab=cloud-computing"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-gray-800 dark:text-gray-200 hover:underline"
-                        >
-                            <h2 className="font-burtons text-2xl py-2 dark:text-white md:text-2xl">
-                                Cloud Computing - Bangkit Academy 2023 by Google, GoTo, Traveloka
-                            </h2>
-                        </a>
-                        <h2 className="font-mono text-base mb-2 dark:text-white md:text-base">
-                            Aug 2023 - Jan 2024 
-                        </h2>
-                        <h2 className="font-mono text-base mb-4 dark:text-white md:text-base">
-                            Bandung, Indonesia - Certified Independent Study (MSIB) - Remote
-                        </h2>
-                        <p className="font-mono text-md leading-8 text-gray-800 dark:text-gray-200 max-w-xl md:text-lg">
-                            •   Lead a team of 6 people from Machine Learning, Cloud Computing, and Android Development for Capstone Project making an Application (CalorEase: Fueling Wellness ‑ Your Personalized Path to Health and Vitality)
-                            <br />
-                            •   Actively participate and contribute in the meeting session and in the project initiatives
-                            <br />
-                            •   Built Application Programming Interface (API) using Express and Node.js for Capstone Project
-                            <br />
-                            •   Deploying API in Google Cloud Platform to become a web‑server
-                        </p>
-                    </div>
-                </div>
 
-                <div className="p-10 py-10 flex flex-col md:flex-row items-start md:items-center">
-                    <div>
-                        <a
-                            href="https://www.dicoding.com/learningpaths/58"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-gray-800 dark:text-gray-200 hover:underline"
-                        >
-                            <h2 className="font-burtons text-2xl py-2 dark:text-white md:text-2xl">
-                                React Developer - Indosat Ooredoo Hutchison Digital Camp
-                            </h2>
-                        </a>
-                        <h2 className="font-mono text-base mb-2 dark:text-white md:text-base">
-                            Sep 2023 - Jan 2024
-                        </h2>
-                        <h2 className="font-mono text-base mb-4 dark:text-white md:text-base">
-                            Indonesia - Remote
-                        </h2>
-                        <p className="font-mono text-md leading-8 text-gray-800 dark:text-gray-200 max-w-xl md:text-lg">
-                            •   Developed web‑based application using React JSX for Dicoding & IDCamp
-                            <br />
-                            •   Completing all of the React Developer Courses in Dicoding Indonesia Platform
-                        </p>
-                    </div>
-                </div>
-                <Footer darkMode={darkMode} />
-                </>
+                        <div className="p-10 py-10 flex flex-col md:flex-row items-start md:items-center">
+                            <div>
+                                <a
+                                    href="https://www.jmc.co.id/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-800 dark:text-gray-200 hover:underline"
+                                >
+                                    <h2 className="font-burtons text-2xl py-2 dark:text-white md:text-2xl">
+                                        Web Programmer - JMC Indonesia
+                                    </h2>
+                                </a>
+                                <h2 className="font-mono text-base mb-2 dark:text-white md:text-base">
+                                    Apr 2024 - Now
+                                </h2>
+                                <h2 className="font-mono text-base mb-4 dark:text-white md:text-base">
+                                    Yogyakarta , Indonesia - Contract
+                                </h2>
+                                <p className="font-mono text-md leading-8 text-gray-800 dark:text-gray-200 max-w-xl md:text-lg">
+                                    •   Build web application like E-Gov, GIS, E-Data for Local Goverment and Private Company using Laravel, Yii2, GIS, and other PHP Frameworks
+                                    <br />
+                                    •   Actively participate and contribute in the meeting session and in the project initiatives
+                                    <br />
+                                    •   Built 2 - 3 Web Applications per month and maintained by the team.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="p-10 py-10 flex flex-col md:flex-row items-start md:items-center">
+                            <div>
+                                <a
+                                    href="https://grow.google/intl/id_id/bangkit/?tab=cloud-computing"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-800 dark:text-gray-200 hover:underline"
+                                >
+                                    <h2 className="font-burtons text-2xl py-2 dark:text-white md:text-2xl">
+                                        Cloud Computing - Bangkit Academy 2023 by Google, GoTo, Traveloka
+                                    </h2>
+                                </a>
+                                <h2 className="font-mono text-base mb-2 dark:text-white md:text-base">
+                                    Aug 2023 - Jan 2024
+                                </h2>
+                                <h2 className="font-mono text-base mb-4 dark:text-white md:text-base">
+                                    Bandung, Indonesia - Certified Independent Study (MSIB) - Remote
+                                </h2>
+                                <p className="font-mono text-md leading-8 text-gray-800 dark:text-gray-200 max-w-xl md:text-lg">
+                                    •   Lead a team of 6 people from Machine Learning, Cloud Computing, and Android Development for Capstone Project making an Application (CalorEase: Fueling Wellness ‑ Your Personalized Path to Health and Vitality)
+                                    <br />
+                                    •   Actively participate and contribute in the meeting session and in the project initiatives
+                                    <br />
+                                    •   Built Application Programming Interface (API) using Express and Node.js for Capstone Project
+                                    <br />
+                                    •   Deploying API in Google Cloud Platform to become a web‑server
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="p-10 py-10 flex flex-col md:flex-row items-start md:items-center">
+                            <div>
+                                <a
+                                    href="https://www.dicoding.com/learningpaths/58"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-gray-800 dark:text-gray-200 hover:underline"
+                                >
+                                    <h2 className="font-burtons text-2xl py-2 dark:text-white md:text-2xl">
+                                        React Developer - Indosat Ooredoo Hutchison Digital Camp
+                                    </h2>
+                                </a>
+                                <h2 className="font-mono text-base mb-2 dark:text-white md:text-base">
+                                    Sep 2023 - Jan 2024
+                                </h2>
+                                <h2 className="font-mono text-base mb-4 dark:text-white md:text-base">
+                                    Indonesia - Remote
+                                </h2>
+                                <p className="font-mono text-md leading-8 text-gray-800 dark:text-gray-200 max-w-xl md:text-lg">
+                                    •   Developed web‑based application using React JSX for Dicoding & IDCamp
+                                    <br />
+                                    •   Completing all of the React Developer Courses in Dicoding Indonesia Platform
+                                </p>
+                            </div>
+                        </div>
+                        <Footer darkMode={darkMode} />
+                    </>
                 )}
             </main>
         </div>
